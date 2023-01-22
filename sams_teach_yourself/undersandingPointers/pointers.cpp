9df5ac973e29764125458f5ff2f7b09c65183059
Tony Daniels <tddanielsjr.com>
2023-01-22 08:59:39 -0600

    1.22.23b

--- sams_teach_yourself/undersandingPointers/pointers.cpp
+++ sams_teach_yourself/undersandingPointers/pointers.cpp
@@ -41,24 +41,25 @@ int main()
     int myAge = 35;
     int *pAge = &myAge;
 
-    std::cout << "1 myage = " << myAge << "\n";     //value
+    std::cout << "1 myAge = " << myAge << "\n";     //value
     std::cout << "2 *pAge = " << *pAge << "\n";     //value
 
     std::cout << "3 pAge = " << pAge << "\n";       //address of myAge (&myAge's address)
+    std::cout<< "4 &myAge = " <<&myAge<<"\n";      //address of myAge
 
     *pAge = 8405;                                   //changing the value of myAge
     pAge = &myAge;                                  //same as line 42 but when redeclaring it, you need not the (*) or else you would be referring to the value of myAge indirectly
 
-    std::cout << "4 *pAge = " << *pAge << "\n";     //*pAge indirectly changed myAge
-    std ::cout << "5 myAge = " << myAge << "\n";    //you see the change that *pAge made  in line 52 and 53
-    std::cout << "3 pAge = " << pAge << "\n";       //address of myAge (&myAge's address) out of those changes, you see the address remains the same
+    std::cout << "5 *pAge = " << *pAge << "\n";     //*pAge indirectly changed myAge
+    std ::cout << "6 myAge = " << myAge << "\n";    //you see the change that *pAge made  in line 52 and 53
+    std::cout << "7 pAge = " << pAge << "\n";       //address of myAge (&myAge's address) out of those changes, you see the address remains the same
 
-    std::cout << "6 so you can indirectly change myAge by changinf the pointer value of *pAge since its pointing to the address of it (myAge)";
-    std::cout << "7 \n"
+    std::cout << "8 so you can indirectly change myAge by changinf the pointer value of *pAge since its pointing to the address of it (myAge)";
+    std::cout << "9 \n"
               << std::endl;
     myAge = 9;
-    std::cout << "8 myAge = " << myAge << "\n";
-    std::cout << "9 *pAge = " << *pAge << "\n";
+    std::cout << "10 myAge = " << myAge << "\n";
+    std::cout << "11 *pAge = " << *pAge << "\n";
 
     std::cout << "10 and you can also change the actual value of myAge which will also be in sync when *pAge points to myAge and displays it\n moral of the story, it can change if you change the value to either";
 }
